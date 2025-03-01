47e9f3ad94d961ee8b9ef59623d2b3e996b5fedb
Nikol_Antiukova <nikol.antiukova@hneu.net>
2025-03-01 13:47:28 +0200

    add main pages for all sections

--- RealAgency/urls.py
+++ RealAgency/urls.py
@@ -1,9 +1,15 @@
 # from django.contrib import admin
 from django.urls import path
 
-from RealAgency.views import home
+from RealAgency.views import home, clients, discounts, payments, provided_services, services
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('clients', clients, name='clients'),
+    path('discounts', discounts, name='discounts'),
+    path('payments', payments, name='payments'),
+    path('clients', clients, name='clients'),
+    path('provided_services', provided_services, name='provided_services'),
+    path('services', services, name='services'),
 ]
--- RealAgency/views.py
+++ RealAgency/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main.html')
+
+def clients(request):
+    return render(request, 'clients/main.html')
+
+def discounts(request):
+    return render(request, 'discounts/main.html')
+
+def payments(request):
+    return render(request, 'payments/main.html')
+
+def provided_services(request):
+    return render(request, 'provided_services/main.html')
+
+def services(request):
+    return render(request, 'services/main.html')
