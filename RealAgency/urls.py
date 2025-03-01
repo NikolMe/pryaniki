bcbb94caf8f09cef843c0c3f52061ea75f09ffb4
Nikol_Antiukova <nikol.antiukova@hneu.net>
2025-03-01 15:38:12 +0200

    add modal window for clients

--- RealAgency/urls.py
+++ RealAgency/urls.py
@@ -1,7 +1,8 @@
 # from django.contrib import admin
 from django.urls import path
 
-from RealAgency.views import home, clients, discounts, payments, provided_services, services
+from RealAgency.views import home, clients, discounts, payments, provided_services, services, \
+    add_or_edit_client, delete_client
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
@@ -12,4 +13,8 @@ urlpatterns = [
     path('clients', clients, name='clients'),
     path('provided_services', provided_services, name='provided_services'),
     path('services', services, name='services'),
+
+    path('add_client/', add_or_edit_client, name='add_client'),
+    path('edit_client/<int:client_id>/', add_or_edit_client, name='edit_client'),
+    path('delete_client/<int:client_id>/', delete_client, name='delete_client'),
 ]
--- RealAgency/views.py
+++ RealAgency/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+
+from RealAgency.models import Client, ClientType
+
 
 def home(request):
     return render(request, 'main.html')
 
 def clients(request):
-    return render(request, 'clients/main.html')
+    client_list = Client.objects.all().order_by('id')
+    paginator = Paginator(client_list, 10)
+
+    client_types = ClientType.objects.all()
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'clients/main.html', {'page_obj': page_obj, 'client_types': client_types })
 
 def discounts(request):
     return render(request, 'discounts/main.html')
@@ -17,3 +29,45 @@ def provided_services(request):
 
 def services(request):
     return render(request, 'services/main.html')
+
+def add_or_edit_client(request, client_id=None):
+    if request.method == 'POST':
+        client_id = request.POST.get('client_id')
+        client_type = ClientType.objects.get(id=request.POST.get('client_type'))
+        name = request.POST.get('name')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        passport_id = request.POST.get('passport_id')
+        iin = request.POST.get('iin')
+        edrpou = request.POST.get('edrpou')
+
+        if client_id:
+            client = get_object_or_404(Client, id=client_id)
+            client.client_type = client_type
+            client.name = name
+            client.address = address
+            client.phone = phone
+            client.email = email
+            client.passport_id = passport_id
+            client.iin = iin
+            client.edrpou = edrpou
+            client.save()
+        else:
+            Client.objects.create(
+                client_type=client_type,
+                name=name,
+                address=address,
+                phone=phone,
+                email=email,
+                passport_id=passport_id,
+                iin=iin,
+                edrpou=edrpou)
+
+        return redirect('clients')
+
+def delete_client(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    client.delete()
+    return JsonResponse({"success": True})
+
